3078cf6a6899aac60219f3f53aacb1fcef16a3d6
Lee Winter <lee.winter@boxspark.co.uk>
2025-02-04 16:51:49 +0000

    Add storybook interactions for most stories

--- lib/components/quill-editor/quill-editor.tsx
+++ lib/components/quill-editor/quill-editor.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-function QuillEditor({ html }: { html?: string }) {
+function QuillEditor({ html, onHtmlChange }: { html?: string, onHtmlChange?: (val: string) => void }) {
   const [content, setContent] = useState(html);
+  const quillRef = useRef<ReactQuill | null>(null);
 
-  return <ReactQuill theme="snow" value={content} onChange={setContent} />;
+  useEffect(() => {
+    if (quillRef.current) {
+      console.log("Quill instance:", quillRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (html !== content) setContent(html);
+  }, [html])
+
+  const handleHtmlUpdate = (val: string) => {
+    if (onHtmlChange) onHtmlChange(val);
+    setContent(val);
+  }
+
+  return <ReactQuill
+    ref={quillRef}
+    data-testid="quill-editor"
+    theme="snow"
+    value={content}
+    onChange={handleHtmlUpdate} />;
 }
 
 export default QuillEditor;
